d30292c8a793cb8421df69a60f985db2d963726d
Michelle Yue Wang <yue.7.wang@kcl.ac.uk>
2022-10-02 19:15:18 +0100

    test

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
+    frequencies = {['a', 'a', 'b', 'b', 'b', 'c']}
     # Your code goes here
-    return frequencies
+    dict = {}
+    for key in items:
+        if key not in dict.keys():
+            dict.update({key: 1})
+        else :
+            dict[key] = dict[key] + 1
+    return dict
